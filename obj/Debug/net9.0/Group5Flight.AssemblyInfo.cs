e53cdd9906b30f3124545cce0ddcbe48998c293e
vaishu29vk <rameshvaishu2020@gmail.com>
2026-04-05 23:07:45 -0500

    fix database for azure

--- obj/Debug/net9.0/Group5Flight.AssemblyInfo.cs
+++ obj/Debug/net9.0/Group5Flight.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group5Flight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18e1a324e6649bae46c7e78f7417b071ff0f258a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group5Flight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group5Flight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
